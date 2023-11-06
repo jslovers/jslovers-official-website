44d8c063e4831d671590de1141a8a543d8645e31
ra-kesh <iamrakeshkumar@pm.me>
2023-11-06 13:32:05 +0530

    chore:colors added to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: {
+          DEFAULT: "#FFF",
+          button: "#AAC6FC",
+          tab: "#CCC",
+          "tab-active": "#F6E258",
+        },
+        border: {
+          DEFAULT: "#000",
+          input: "#939393",
+          heading: "#505050",
+          card: "#383A48",
+        },
+        text: {
+          DEFAULT: "#000",
+          paragraph: "#505050",
+          muted: "#696969",
+          "sub-heading": "#383A48",
+          accent: "#1E4CA1",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
