8aee31a57f033907fd51126585fd26f53328482a
ra-kesh <iamrakeshkumar@pm.me>
2023-11-08 12:07:32 +0530

    feat:type checking for footer items

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import { Icons } from "./Icons";
 import { FOOTER } from "@/config/footer";
+import { FooterItem } from "@/types";
 
 //TODO: Redesign may be needed according to the finalised footer contnent
 
 export function Footer({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={className}>
-      <h2 className="sr-only">Footer</h2>
+      <h5 className="sr-only">Footer</h5>
       <div className="container px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32 lg:pb-16">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-4">
@@ -20,8 +21,8 @@ export function Footer({ className }: React.HTMLAttributes<HTMLElement>) {
             <div className="md:grid md:grid-cols-3 ">
               <div className="text-center">
                 <h3 className="text-lg font-medium leading-6">Quick Links</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {FOOTER.QuickLinks.map((item) => (
+                <ul className="mt-6 space-y-4">
+                  {FOOTER.QUICK_LINKS.map((item: FooterItem) => (
                     <li key={item.TITLE}>
                       <Link href={item.HREF}>{item.TITLE}</Link>
                     </li>
@@ -30,8 +31,8 @@ export function Footer({ className }: React.HTMLAttributes<HTMLElement>) {
               </div>
               <div className="text-center">
                 <h3 className="text-lg font-medium leading-6">Follow Us</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {FOOTER.FollowUs.map((item) => (
+                <ul className="mt-6 space-y-4">
+                  {FOOTER.FOLLOW_US.map((item: FooterItem) => (
                     <li key={item.TITLE}>
                       <Link href={item.HREF}>{item.TITLE}</Link>
                     </li>
@@ -40,8 +41,8 @@ export function Footer({ className }: React.HTMLAttributes<HTMLElement>) {
               </div>
               <div className="text-center">
                 <h3 className="text-lg font-medium leading-6">Sponsors</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {FOOTER.Sponsors.map((item) => {
+                <ul className="mt-6 space-y-4">
+                  {FOOTER.SPONSORS.map((item: FooterItem) => {
                     return (
                       <li key={item.TITLE} className="flex justify-center">
                         <span className="sr-only">{item.TITLE}</span>
--- types.ts
+++ types.ts
@@ -2,3 +2,7 @@ export type NavItem = {
   TITLE: string;
   HREF: string;
 };
+
+export type FooterItem = NavItem & {
+  LOGO?: JSX.Element;
+};
