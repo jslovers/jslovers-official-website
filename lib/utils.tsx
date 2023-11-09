020262e5af3eb9c7e417b5e5c26ca2e54f5310e1
ra-kesh <iamrakeshkumar@pm.me>
2023-11-09 13:44:40 +0530

    feat;added a utility for generating underline svgs

---
+++ lib/utils.tsx
@@ -0,0 +1,40 @@
+import clsx from "clsx";
+import { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function generateUnderlineSVGs(widths: number[], heights: number[]) {
+  const svgs = [];
+
+  for (let i = 0; i < widths.length; i++) {
+    const width = widths[i];
+    const height = heights[i];
+
+    const svg = (
+      <svg
+        key={i}
+        width={width}
+        height={height}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="1.5"
+          y="1.5"
+          width={width - 3}
+          height={height - 3}
+          rx="2.5"
+          fill="white"
+          stroke="#505050"
+          strokeWidth="3"
+        />
+      </svg>
+    );
+
+    svgs.push(svg);
+  }
+
+  return svgs;
+}
