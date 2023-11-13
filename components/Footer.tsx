6efe943ccda8de392e6033ca61b2e8643b58e448
ra-kesh <iamrakeshkumar@pm.me>
2023-11-14 01:04:14 +0530

    fix:footer mobile view

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,27 @@
 import Link from "next/link";
 import { Icons } from "./Icons";
 import { FOOTER } from "@/config/footer";
+import { cn } from "@/lib/utils";
 
 //TODO: Redesign may be needed according to the finalised footer contnent
 
 export function Footer({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
-    <footer className={className}>
+    <footer className={cn(className)}>
       <h5 className="sr-only">Footer</h5>
       <div className="container px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32 lg:pb-16">
-        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div className="lg:grid lg:grid-cols-3 lg:gap-8">
           <div className="space-y-4">
             <Icons.logo />
             <p className="text-normal leading-6">
               Community for developers by developers.
             </p>
           </div>
-          <div className="mt-16 grid grid-cols-1 gap-8 xl:col-span-2 xl:mt-0">
-            <div className="md:grid md:grid-cols-3 ">
-              <div className="text-center">
+          <div className="mt-6 grid grid-cols-1 gap-8 lg:col-span-2 lg:mt-0">
+            <div className="md:grid md:grid-cols-3 space-y-6 md:space-y-0">
+              <div className="lg:text-center">
                 <h3 className="text-lg font-medium leading-6">Quick Links</h3>
-                <ul className="mt-6 space-y-4">
+                <ul className="mt-4 space-y-4">
                   {FOOTER.QuickLinks.map((item) => (
                     <li key={item.title}>
                       <Link href={item.href || ""}>{item.title}</Link>
@@ -28,9 +29,9 @@ export function Footer({ className }: React.HTMLAttributes<HTMLElement>) {
                   ))}
                 </ul>
               </div>
-              <div className="text-center">
+              <div className="lg:text-center">
                 <h3 className="text-lg font-medium leading-6">Follow Us</h3>
-                <ul className="mt-6 space-y-4">
+                <ul className="mt-4 space-y-4">
                   {FOOTER.FollowUs.map((item) => (
                     <li key={item.title}>
                       <Link href={item.href || ""}>{item.title}</Link>
@@ -38,12 +39,12 @@ export function Footer({ className }: React.HTMLAttributes<HTMLElement>) {
                   ))}
                 </ul>
               </div>
-              <div className="text-center">
+              <div className="lg:text-center">
                 <h3 className="text-lg font-medium leading-6">Sponsors</h3>
-                <ul className="mt-6 space-y-4">
+                <ul className="mt-4 space-y-4">
                   {FOOTER.Sponsors.map((item) => {
                     return (
-                      <li key={item.title} className="flex justify-center">
+                      <li key={item.title} className="flex lg:justify-center">
                         <span className="sr-only">{item.title}</span>
                         {item.logo}
                       </li>
