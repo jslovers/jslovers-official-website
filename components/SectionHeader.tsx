257af73c8afe68b76e238e2efeacfe7ec0a1d6ad
ra-kesh <iamrakeshkumar@pm.me>
2023-11-14 00:15:18 +0530

    style:mobile font for section header

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -9,13 +9,24 @@ interface SectionHeaderProps {
 }
 
 const SectionHeader = ({ header, className }: SectionHeaderProps) => {
-  const widths = [266, 54, 54, 54];
-  const heights = [12, 12, 12, 12];
+  const desktopWidths = [266, 54, 54, 54];
+  const desktopHeights = [12, 12, 12, 12];
+  const mobileWidths = [139, 28, 28];
+  const mobileHeights = [12, 12, 12];
 
   return (
     <div className={cn("flex flex-col", className)}>
-      <h3 className="text-[40px] font-semibold text-right">{header}</h3>
-      <div className="flex gap-2">{generateUnderlineSVGs(widths, heights)}</div>
+      <h3 className="text-2xl lg:text-[40px] font-semibold text-left lg:text-right">
+        {header}
+      </h3>
+      <div>
+        <div className="hidden lg:flex gap-2">
+          {generateUnderlineSVGs(desktopWidths, desktopHeights)}
+        </div>
+        <div className="flex gap-1 lg:hidden ">
+          {generateUnderlineSVGs(mobileWidths, mobileHeights)}
+        </div>
+      </div>
     </div>
   );
 };
