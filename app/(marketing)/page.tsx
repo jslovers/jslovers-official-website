309409e7d8ed687664e0a2c5107e2f361dd4e2de
ra-kesh <iamrakeshkumar@pm.me>
2023-11-13 13:46:25 +0530

    style: initial hero section mobile view

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -12,8 +12,8 @@ export default async function IndexPage() {
           <div className="max-w-2xl justify-between lg:flex lg:max-w-full lg:items-center gap-x-20">
             {/* todo:make the below a separate dumb component  */}
 
-            <div className="w-1/2 max-w-2xl lg:shrink-0 ">
-              <div className="flex items-center group flex-wrap">
+            <div className="w-full lg:w-1/2 max-w-2xl lg:shrink-0 ">
+              <div className="flex justify-center lg:justify-start items-center group flex-wrap">
                 <Image
                   src="/assets/hero_image_guest_v1.png"
                   width="149"
@@ -22,8 +22,8 @@ export default async function IndexPage() {
                   className="object-cover "
                   loading="lazy"
                 />
-                <div className="ml-5">
-                  <h2 className="text-3xl font-medium text-text-sub-heading-2 mb-2">
+                <div className="lg:ml-5 text-center lg:text-left">
+                  <h2 className="text-2xl lg:text-3xl font-medium text-text-sub-heading-2 mb-2">
                     Upcoming Workshop by
                   </h2>
                   <p className="text-xl font-semibold">John Doe</p>
@@ -32,17 +32,20 @@ export default async function IndexPage() {
                   </p>
                 </div>
               </div>
-              <h1 className="mt-2 text-[40px] font-semibold leading-tight">
+              <h1 className="mt-2 text-center lg:text-left text-[40px] font-semibold leading-tight">
                 Introduction To Design System and Storybook
               </h1>
-              <p className="relative mt-2 text-lg text-text-paragraph sm:max-w-md lg:max-w-none">
+              <p className="relative mt-2 text-center lg:text-left text-lg text-text-paragraph sm:max-w-md lg:max-w-none">
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint. Velit officia consequat duis enim velit mollit.
                 Exercitation veniam consequat sunt nostrud amet.
               </p>
-              <p className="mt-3 font-semibold text-text-accent">
+              <p className="mt-3 font-semibold text-text-accent text-center lg:text-left">
                 On 24th October at 05 : 00 AM IST
               </p>
+
+              {/* todo:mobile view  */}
+
               <form className="mt-6 flex items-center">
                 <label htmlFor="email-address" className="sr-only">
                   Email address
@@ -55,12 +58,14 @@ export default async function IndexPage() {
                     autoComplete="email"
                     placeholder="Enter your email"
                     required
-                    className="flex-auto w-full rounded-lg border-0  sm:text-sm sm:leading-6 ring-2 ring-inset ring-border-input h-16 pl-6 placeholder:text-lg placeholder:text-"
+                    className="flex-auto w-full rounded-lg border-0  sm:text-sm sm:leading-6 ring-2 ring-inset ring-border-input  h-16 pl-6 placeholder:text-lg placeholder:text-"
                   />
                   <button
                     type="submit"
                     className={cn(
-                      buttonVariants({ size: "lg" }),
+                      buttonVariants({
+                        size: "lg",
+                      }),
                       "absolute inset-y-0 right-0 border-solid border-2 border-border text-lg "
                     )}
                   >
